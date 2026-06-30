d03a43267965cb0ab1663d2ca37b86a8bb515c1e
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2026-06-30 08:19:49 +0200

    fix: update homebrew cask with real SHA256 hashes for v0.2.1

--- homebrew-tap/Casks/better-screenshoot.rb
+++ homebrew-tap/Casks/better-screenshoot.rb
@@ -3,12 +3,12 @@ cask "better-screenshoot" do
 
   on_arm do
     url "https://github.com/sriverogalan/better-screenshoot/releases/download/v#{version}/Better-Screenshoot_#{version}_aarch64.dmg"
-    sha256 "PLACEHOLDER_ARM64_SHA256"
+    sha256 "b3d425c087c2706f858381d3f6e7f8beef4c59c16a7e2006d4dd79bfc042eaa9"
   end
 
   on_intel do
     url "https://github.com/sriverogalan/better-screenshoot/releases/download/v#{version}/Better-Screenshoot_#{version}_x64.dmg"
-    sha256 "PLACEHOLDER_X64_SHA256"
+    sha256 "722105dcbb5578a2f02166906b7842c1673d503586569e655d6512040b835b71"
   end
 
   name "Better Screenshoot"
